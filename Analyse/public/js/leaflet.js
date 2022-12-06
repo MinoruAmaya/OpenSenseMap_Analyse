f80c3f588170c03d53585a51c57559f921bb25cd
Jonathan Mader <96208636+JonthMM@users.noreply.github.com>
2022-12-06 18:30:04 +0100

    Delete Analyse directory

--- Analyse/public/js/leaflet.js
+++
@@ -1,5 +0,0 @@
-var map = L.map('map').setView([51.97, 7.63], 12);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);

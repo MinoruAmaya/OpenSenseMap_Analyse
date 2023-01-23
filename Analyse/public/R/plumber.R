131d21c4fa3d1899afe3d1ce3876f61fd00d18d0
Jonathan Mader <jonathanmadr16@gmail.com>
2023-01-23 12:59:19 +0100

    layout changes, grammar fixes, plot view fixed

--- Analyse/public/R/plumber.R
+++ Analyse/public/R/plumber.R
@@ -35,12 +35,12 @@ nightPlot <- function(Messungen){
 #from <- "2022-11-22T08:00:00Z"
 #to <- "2022-11-23T08:00:00Z"
 #phenomenon <- "Lautst%C3%A4rke"
-#structurePlot("60f077874fb91e001c71b3b1", "2022-11-22T08:00:00Z", "2022-11-23T08:00:00Z",FALSE, TRUE, FALSE, FALSE, 1, 1)
+#structurePlot("60f077874fb91e001c71b3b1", "2022-11-22T08:00:00Z", "2022-11-23T08:00:00Z",TRUE, TRUE, TRUE, TRUE)
 
 #* Plottet die Werte nach Wunsch des Kunden
 #* @serializer png
 #* @get /structurePlot
-structurePlot <- function(boxID, from, to, Day, Night, MA, Mean, colors, theme){
+structurePlot <- function(boxID, from, to, Day, Night, MA, Mean){
   
   # Einladen Der Daten
   openSenseBox <- sprintf("https://api.opensensemap.org/boxes/data?boxId=%s&from-date=%s&to-date=%s&phenomenon=", boxID, from, to)
@@ -48,23 +48,23 @@ structurePlot <- function(boxID, from, to, Day, Night, MA, Mean, colors, theme){
   Messungen <- read.csv(openSenseBox)
   Messungen$createdAt <- as.POSIXct(Messungen$createdAt,format="%Y-%m-%dT%H:%M:%S",tz=Sys.timezone())
   Ausgabe <- ggplot(data = Messungen, aes(x=createdAt, y=value))
-  counter <- 1
-  
+  #counter <- 1
+  colors <- c("Scatter Plot" = "darkgreen", "Mittelwert" = "darkblue", "Moving Average" = "yellow")
   
   if(Day == TRUE){
     if(Night == TRUE){
       
       print("Kompletter Tag!")
-      Ausgabe <- Ausgabe + geom_point(col = "red")
-      counter <- counter + 1
+      Ausgabe <- Ausgabe + geom_point(aes(col = "Scatter Plot"), size=0.5)
+      #counter <- counter + 1
       
     } else {
       
       print("Nur Tagsüber!")
       Messungen <- dayPlot(Messungen)
       Ausgabe <- ggplot(data = Messungen, aes(x=createdAt, y=value))
-      Ausgabe <- Ausgabe + geom_point(col= "red")
-      counter <- counter + 1
+      Ausgabe <- Ausgabe + geom_point(aes(col = "Scatter Plot"), size=0.5)
+      #counter <- counter + 1
       
     }
   } else {
@@ -73,8 +73,8 @@ structurePlot <- function(boxID, from, to, Day, Night, MA, Mean, colors, theme){
       print("Nur Nachts!")
       Messungen <- nightPlot(Messungen)
       Ausgabe <- ggplot(data = Messungen, aes(x=createdAt, y=value))
-      Ausgabe <- Ausgabe + geom_point(col= "red")
-      counter <- counter + 1
+      Ausgabe <- Ausgabe + geom_point(aes(col = "Scatter Plot"), size=0.5)
+      #counter <- counter + 1
       
     } else {
       
@@ -87,25 +87,26 @@ structurePlot <- function(boxID, from, to, Day, Night, MA, Mean, colors, theme){
   if(Mean == TRUE){
     Messungen <- Messungen %>%
       mutate(mean_value = mean(value))
-    Ausgabe <- Ausgabe + geom_line(data = Messungen, aes(createdAt,mean_value), size=2, col = "blue")
-    counter <- counter + 1
+    Ausgabe <- Ausgabe + geom_line(data = Messungen, aes(createdAt,mean_value,col = "Mittelwert"), size=2)
+    #counter <- counter + 1
   }
   
   if(MA == TRUE){
-    Ausgabe <- Ausgabe + geom_smooth(method = "loess", col = "green")
-    counter <- counter + 1
+    Ausgabe <- Ausgabe + geom_smooth(aes(col = "Moving Average"), method = "loess")
+    #counter <- counter + 1
   }
   
   Ausgabe <- Ausgabe + theme_bw()+ theme(
     
-    panel.border = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
+    #panel.border = element_blank(),
+    #panel.grid.major = element_blank(),
+    #panel.grid.minor = element_blank(),
     
-    axis.line = element_line(colour = "black"),
+    #axis.line = element_line(colour = "black"),
   ) 
   
-  Ausgabe <- Ausgabe + labs(x = "Zeit der Messung", y = "Lautstärke (Db)")
+  Ausgabe <- Ausgabe + labs(x = "Zeitpunkt der Messung", y = "Lautstärke (Db)", color = "Legende")
+  Ausgabe <- Ausgabe + scale_color_manual(values = colors)
   print(Ausgabe)
   
   predictionPlot()

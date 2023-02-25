41e42f7f6cd97f170ac3fc9ddd43348f8fd70441
Jonathan Mader <96208636+JonthMM@users.noreply.github.com>
2023-02-25 23:08:56 +0100

    Delete Analyse directory

--- Analyse/public/js/kalibrierung.js
+++
@@ -1,5 +0,0 @@
-let btn_asTeil = document.getElementById('btn_asTeil')
-let btn_asLead = document.getElementById('btn_asLead')
-
-btn_asTeil.addEventListener('click', function(){window.location = 'kalibrierung/kal-atten'})
-btn_asLead.addEventListener('click', function(){window.location = 'kalibrierung/kal-lead'})
